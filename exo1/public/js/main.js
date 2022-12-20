b1257bb6c9b555e5f9823b18673ea34705fdf461
DevEless <qdelameilleure@gmaile.com>
2022-12-20 10:23:12 +0100

    exo 1

---
+++ exo1/public/js/main.js
@@ -0,0 +1,13 @@
+let courses = ["coding 20", "coding 21", "coding 22"];
+
+console.log(courses);
+console.log(courses.length);
+
+for (let course of courses) {
+  console.log(course);
+}
+
+for (let i = 0; i < courses.length; i++) {
+  console.log(courses[i]);
+}
+
